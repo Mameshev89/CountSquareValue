2d26d927f7b4348961b0b9cb02abfbbea63da6b0
Ilnur Mameshev <imameshev@inbox.ru>
2021-10-30 19:59:38 +0300

    add some test case

---
+++ src/main/java/SQRService.java
@@ -0,0 +1,12 @@
+public class SQRService {
+    public int countSquareValue(int lowLim, int uplim) {
+        int count = 0;
+        for (int i = 10; i < 99; i++) {
+            int sqr = i * i;
+            if (sqr >= lowLim && sqr <= uplim) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
